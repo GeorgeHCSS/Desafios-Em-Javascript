3899c5736a35babc60fbcbba9db1d6417b8f37cc
George Henrique <88684410+GeorgeHCSS@users.noreply.github.com>
2021-10-21 00:36:09 -0300

    Update index.js
    Adição de Texto do Desafio

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Em uma disputa de tiro, uma Catapulta, operando durante 5 Rodadas de 15 minutos cada, lança 300 pedras. Quantas pedras lançará em 8 rodadas de 7 minutos cada?
 
 //Inicio da Rodada
 rodadas = 5
