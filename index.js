4770bc938b0678d424813f120ba3ad9ebb3a65a8
GeorgeHCSS <george.henriquess@hotmail.com>
2021-10-21 01:27:07 -0200

    Arquivo JS

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+//Inicio da Rodada
+rodadas = 5
+duracao = 15
+pedras = 300
+
+//Segunda Rodada
+rodadas2 = 8
+duracao2 = 7
+pedras2 = ((rodadas2 * duracao2)*pedras)/(rodadas*duracao)
+
+console.log("A catapulta lançou "+ pedras2 +" pedras em "+rodadas2+" rodadas de "+duracao2+" minutos.")
+
+
